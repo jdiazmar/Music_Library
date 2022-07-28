5cdbfd2b8dcc2e6c429824ba4a82ae863b6d7c26
Josmar Diaz <jdm.begreat28@gmail.com>
2022-07-28 19:33:17 -0400

    Created useEffect + useState + Async Functions

--- music-library/src/App.js
+++ music-library/src/App.js
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function App() {
+
+const [songs, setSongs] = useState([]);
+
+useEffect(() => {
+    getAllSongs();
+}, []);
+
+async function getAllSongs(){
+  const response = await axios.get('http://127.0.0.1:8000/music/');
+  console.log(response.data);
+  setSongs(response.data);
+}
   return (
     <div>
-      
+      <button onClick={() => getAllSongs()}>Get All Songs!</button>
     </div>
   );
 }
