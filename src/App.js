8fa9d7513dc341b3b6efc0582525688c917befb7
Josmar Diaz <jdm.begreat28@gmail.com>
2022-08-02 21:54:51 -0400

    Created New Component to AddNewSong + Created Functionality For DisplayMusic and Started To Create HTML Table Within Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import DisplayMusic from './Components/DisplayMusic/DisplayMusic';
+import AddNewSong from './Components/AddNewSong/AddNewSong';
 
 function App() {
 
@@ -14,8 +16,22 @@ async function getAllSongs(){
   console.log(response.data);
   setSongs(response.data);
 }
+
+const [entries, setEntries] = useState([{title: 'Painting Pictures', artist: 'Tj Carrol', album: 'Painting Pictures', release_date: '2022-07-28', genre: 'Hip-Hop'}])
+
+function addNewSong(entry) {
+  let tempEntries = [entry, ...entries];
+  setEntries(tempEntries);
+}
   return (
+    
     <div>
+      <div>
+        <DisplayMusic userEntries={entries}/>
+      </div>
+      <div>
+        <AddNewSong addNewSongProperty={addNewSong}/>
+      </div>
       <button onClick={() => getAllSongs()}>Get All Songs!</button>
     </div>
   );
---
+++ src/Components/DisplayMusic/DisplayMusic.jsx
@@ -0,0 +1,30 @@
+const DisplayMusic = (props) => {
+    return ( 
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Release Date</th>
+                    <th>Genre</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.userEntries.map((entry) => {
+                    return (
+                        <tr>
+                            <td>{entry.title}</td>
+                            <td>{entry.artist}</td>
+                            <td>{entry.album}</td>
+                            <td>{entry.release_date}</td>
+                            <td>{entry.genre}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+     );
+}
+ 
+export default DisplayMusic;
