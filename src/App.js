67f869bc6dbd71ea6bbe7445f4cd37d65eb4b673
Josmar Diaz <jdm.begreat28@gmail.com>
2022-08-04 19:04:33 -0400

    Rearranged Functions in DisplayMusic Component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import DisplayMusic from './Components/DisplayMusic/DisplayMusic';
 import AddNewSong from './Components/AddNewSong/AddNewSong';
 
 function App() {
 
-  const [songs, setSongs] = useState([]);
-
-  useEffect(() => {
-      getAllSongs();
-  }, []);
-  
-  async function getAllSongs(){
-    const response = await axios.get('http://127.0.0.1:8000/music/');
-    console.log(response.data);
-    setSongs(response.data);
-  }
-  
   const [entries, setEntries] = useState([{title: 'Painting Pictures', artist: 'Tj Carrol', album: 'Painting Pictures', release_date: '2022-07-28', genre: 'Hip-Hop'}])
-  
+    
   function addNewSong(entry) {
     let tempEntries = [entry, ...entries];
     setEntries(tempEntries);
 
   }
 
+
   return (
     
     <div>
       <div>
-        <DisplayMusic userEntries={entries}/>
+        <DisplayMusic userEntries={entries} />
       </div>
       <div>
         <AddNewSong addNewSongProperty={addNewSong}/>
       </div>
-      <button onClick={() => getAllSongs()}>Get All Songs!</button>
+     
     </div>
   );
 }
--- src/Components/DisplayMusic/DisplayMusic.jsx
+++ src/Components/DisplayMusic/DisplayMusic.jsx
@@ -1,8 +1,24 @@
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const DisplayMusic = (props) => {
 
+    const [songs, setSongs] = useState([]);
+
+    useEffect(() => {
+        getAllSongs();
+    }, []);
+    
+    async function getAllSongs(){
+      const response = await axios.get('http://127.0.0.1:8000/music/');
+      console.log(response.data);
+      setSongs(response.data);
+    }
+    
+ 
+
 return ( 
+    <div>
         <table>
             <thead>
                 <tr>
@@ -16,6 +32,7 @@ return (
             <tbody>
                 {props.userEntries.map((entry) => {
                     return (
+
                         <tr>
                             <td>{entry.title}</td>
                             <td>{entry.artist}</td>
@@ -26,8 +43,13 @@ return (
                     )
                 })}
             </tbody>
+            <div>   
+                <button onClick={() => getAllSongs()}>Get All Songs!</button>
+            </div>
         </table>
+    </div>
      );
 }
  
 export default DisplayMusic;
+
