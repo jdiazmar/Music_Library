c839cd65a5288cff9aeeca781b64abfb82c69e7e
Josmar Diaz <jdm.begreat28@gmail.com>
2022-08-09 21:55:19 -0400

    Restarting AddNewSong + SearchBar Components

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import DisplayMusic from './Components/DisplayMusic/DisplayMusic';
-import AddNewSong from './Components/AddNewSong/AddNewSong';
+
+
 
 function App() {
 
@@ -17,22 +18,12 @@ function App() {
     setSongs(response.data);
   }
 
-  const [entries, setEntries] = useState([])
-
-  function addNewSong(entry) {
-  let userEntries = [...entries, entry];
-  setEntries(userEntries);
   
-} 
-  
-
-
   return (
     
     <div>
       <div>
         <DisplayMusic parentEntries={songs}  />
-        <AddNewSong AddNewSongProperty={addNewSong} />
       </div>
      </div>
   );
