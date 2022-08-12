3e9a26715caaaed77fc4180c6dd6659260b89c9e
Josmar Diaz <jdm.begreat28@gmail.com>
2022-08-12 17:46:45 -0400

    Finished AddNewSong Function That Allows New Song Entries to Be Displayed

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   useEffect(() => {
       getAllSongs();
-  }, []);
+  }, [addSong]);
   
   async function getAllSongs(){
     const response = await axios.get('http://127.0.0.1:8000/music/');
@@ -19,17 +19,13 @@ function App() {
     setSongs(response.data);
   };
 
- const [entries, setEntries] = useState([])
 
-  useEffect(() => {
-    userEntry();
-  });
 
-  async function userEntry(){
-    let response = await axios.post('http://127.0.0.1:8000/music/', ...entries)
-      console.log(response.data);
-      setEntries(response.data);
-  }
+  async function addSong(newSong){
+    const response = await axios.post('http://127.0.0.1:8000/music/', newSong)
+    console.log(response.data);
+    return response.data;
+  };
   
  
   
@@ -38,7 +34,7 @@ function App() {
     <div>
       <div>
         <DisplayMusic parentEntries={songs}  />
-        <AddNewSong addNewSongProperty={userEntry}  />
+        <AddNewSong addNewSongProperty={addSong}  />
       </div>
      </div>
   );
--- Frontend/src/Components/AddNewSong/AddNewSong.jsx
+++ Frontend/src/Components/AddNewSong/AddNewSong.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
 
-const AddNewSong = (props) => {
+
+const AddNewSong = ({addNewSongProperty}) => {
 
 const [title, setTitle] = useState('');
 const [artist, setArtist] = useState('');
 const [album, setAlbum] = useState('');
-const [releaseDate, setReleaseDate] = useState('');
+const [release_date, setRelease_Date] = useState('');
 const [genre, setGenre] = useState('');
 
 function handleSubmit(event) {
@@ -15,11 +16,19 @@ function handleSubmit(event) {
         title: title,
         artist: artist,
         album: album,    
-        releaseDate: releaseDate,
+        release_date: release_date,
         genre: genre
     };
-    console.log(newSong);
-    props.addNewSongProperty(newSong);
+
+    addNewSongProperty(newSong);
+    // axios
+    //     .post(
+    //         'http://127.0.0.1:8000/music/post.json', 
+    //         newSong,
+    //     )
+    //     .then((response) => {
+    //         console.log(response);
+    //     });
 }
 
 
@@ -37,7 +46,7 @@ function handleSubmit(event) {
             <input type='text'  value={album} onChange={(event) => setAlbum(event.target.value)} />
        
             <label>Release Date</label>
-            <input type='date'  value={releaseDate} onChange={(event) => setReleaseDate(event.target.value)} />
+            <input type='date'  value={release_date} onChange={(event) => setRelease_Date(event.target.value)} />
       
             <label>Genre</label>
             <input type='text'  value={genre} onChange={(event) => setGenre(event.target.value)} />
