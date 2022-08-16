55816422ed780dd4a5adc0d8d66cebcb48ee2429
Josmar Diaz <jdm.begreat28@gmail.com>
2022-08-16 18:04:50 -0400

    Created Delete Button Functionallity But Still Finalizing It

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -19,12 +19,15 @@ function App() {
     setSongs(response.data);
   };
 
-
-
   async function addSong(newSong){
-    const response = await axios.post('http://127.0.0.1:8000/music/', newSong)
+    const response = await axios.post('http://127.0.0.1:8000/music/', newSong);
     return response.data;
   };
+
+  async function deleteSong(entry){
+    let response = await axios.delete('http://127.0.0.1:8000/music/', entry);
+    return response.data;
+  }
   
  
   
@@ -32,7 +35,7 @@ function App() {
     
     <div>
       <div>
-        <DisplayMusic parentEntries={songs}  />
+        <DisplayMusic parentEntries={songs} deleteSongProperty={deleteSong} />
         <AddNewSong addNewSongProperty={addSong}  />
       </div>
      </div>
