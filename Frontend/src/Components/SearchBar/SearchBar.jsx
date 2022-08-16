9311fc3a3a8774de0c9e8558a209c3e2d5ce7d4c
Josmar Diaz <jdm.begreat28@gmail.com>
2022-08-15 21:27:13 -0400

    Started on SearchBar Component

---
+++ Frontend/src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+
+const SearchBar = (props) => {
+
+    const [searchSongs, setSearchSongs] = useState('');
+    
+
+
+    return (  );
+}
+ 
+export default SearchBar;
